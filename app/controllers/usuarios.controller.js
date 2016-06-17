2e9da8a352941545d87dc6d5b9cd6830ad5927b4
JorgePuente <jorge.alejandro.puente@gmail.com>
2016-06-17 11:29:13 -0500

    Usuarios:  - Facebook users

--- app/controllers/usuarios.controller.js
+++ app/controllers/usuarios.controller.js
@@ -10,6 +10,33 @@ exports.create = function(req, res) {
 exports.authenticate = function(req, res){
 	var params = req.body.conditions;
     Usuario.find({ uid : params.uid }, function(err, result) {
-        console.log(result);
+    	if (err) {
+    		res.send(err);
+    	}else{
+	        if (res.length > 0) {
+	        	res.json({ success: true , new_user : false});  
+	        } else {
+	        	new_user(params);
+	        }
+    	}
     })
+}
+
+function new_user(params) {
+	var usuario = new Usuario();
+
+	usuario.nombre = params.nombre;
+	usuario.uid = params.uid;
+	usuario.url_imagen = param.picture_url;
+	usuario.createdBy = 'Admin';
+	usuario.modifiedBy = 'Admin';
+
+
+	usuario.save(function(err) {
+	    if(err){
+	        res.send(err);
+	    }else{
+	        res.json({ success: true, new_user : true});  
+	    }
+	});
 }
